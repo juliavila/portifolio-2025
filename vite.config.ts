766c75e326a66d4b21c336ae6f50cb3a6c8356e6
Júlia Vila de Lima <juliafvila@gmail.com>
2025-04-30 22:26:33 -0300

    feat: adicionando base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portifolio-2025/',
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: 'parens-division',
+      },
+      scss: {
+        api: 'modern-compiler', // or "modern", "legacy"
+        importers: [
+          // ...
+        ],
+      },
+    },
+  },
 })
