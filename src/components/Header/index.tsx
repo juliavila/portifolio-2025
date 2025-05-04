5ad03fb693cbdfd0bb074ae154ea662418493774
Júlia Vila de Lima <juliafvila@gmail.com>
2025-05-04 19:22:12 -0300

    feat: removendo foto do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,21 @@
 import { useContext } from 'react';
-import styles from './styles.module.scss';
 import { InternacionalizationContext } from '../../context/internacionalizationContext';
 import texts from '../../internationalization/texts';
 import LanguageToogle from '../LanguageToogle';
+
+import styles from './styles.module.scss';
 
 function Header({ goToHome }: { goToHome: () => void }) {
   const { language } = useContext(InternacionalizationContext);
 
   return <div className={styles.header}>
-    <div className={styles.pic} onClick={goToHome}></div>
-
-    <div className={styles.actions}>
-      <LanguageToogle />
-
-      <h3
-        className={styles.title}
-        onClick={goToHome}
-      >
-        {texts.header[language]}
-      </h3>
-    </div>
+    <h3
+      className={styles.title}
+      onClick={goToHome}
+    >
+      {texts.header[language]}
+    </h3>
+    <LanguageToogle />
   </div>
 }
 
